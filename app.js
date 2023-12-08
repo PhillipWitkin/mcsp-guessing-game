9e25e0b4f9b3abd27064838c9454d065d6b44ef7
PhillipWitkin <pwitkin273@gmail.com>
2023-12-08 12:40:39 -0500

    feature 1 complete

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-// const userName = prompt("What is your name?");
+console.log("app.js started");
 
-function play() {
-  let secretNumber = Math.floor(Math.random() * 10) + 1;
-  let prevGuesses = [];
-  while(true) {
-    const guess = prompt("Guess a number.");
-    if(guess === null) {
-      alert("Goodbye!");
-      break;
-    }
+const minGuess = 1;
+const maxGuess = 10;
 
-    const numGuess = Number(guess);
-    
-    if(Number.isNaN(numGuess)) {
-      alert("Please enter a valid number");
-    } else if(numGuess < secretNumber) {
-      prevGuesses.push(numGuess);
-      alert(`Sorry ${userName}, guess higher`);
-    } else if(numGuess > secretNumber) {
-      prevGuesses.push(numGuess);
-      alert(`Sorry ${userName}, guess lower`);
-    } else {
-      alert(`Correct! Your previous guess(es) were ${prevGuesses.join(", ")}.`);
-      const playAgain = prompt("Do you want to play again?");
-      if(playAgain.toLowerCase() === "yes") {
-        play();
-      } else {
-        break;
-      }
+
+// create a random number called 'target'
+const numTarget = getRandomIntInclusive(minGuess, maxGuess);
+
+// guessOnce();
+
+
+// prompt user for a number, return a string representing if the number is greater, less or equal to a target
+// function guessOnce(){
+    // use prompt to get user input
+    // let input = prompt("Guess a number between " + maxGuess + " and " + maxGuess);
+    let input = prompt(`Guess a number between ${minGuess} maxGuess and ${maxGuess}`);
+    console.log(`The user guessed: ${input}`);
+    // compare user guess to target
+    let inputNum = parseInt(input); // convert from string to integer
+    console.log("input as number: ", inputNum);
+    // inform user if guess is >, <, or = to target
+    if (inputNum < numTarget){
+        alert("Lower");
+    } else if (inputNum > numTarget){
+        alert("Greater");
+    } else { // they are equal
+        alert("Correct!");
     }
-  }
+// }
+
+console.log("The number was ", numTarget);
+
+
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-
-play();
